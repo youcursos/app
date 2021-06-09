377f2dfd0780fee0b1c6dc6cf422f08aa57a7954
youcursos <84543261+youcursos@users.noreply.github.com>
2021-06-09 20:04:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ function loadJson() {
         method: "get"
     }).then(function(resposta) {
         var recebe = document.querySelector("#recebe")
+
         resposta.json().then(function(data) {
             let dados = data.contente
+            var campoBusca = document.querySelector("#campo").value
+            recebe.innerHTML = ""
             for (let i = 0; i < dados.length; i++)
+            /* Busca */
+                if (dados[i].tags.includes(campoBusca)) {
+
                 recebe.innerHTML += `<div class="cardCursos pb-1">
                 <a href="${dados[i].link}">
                     <img src="${dados[i].linkImg}" class="card-img-top " alt="... ">
@@ -13,10 +19,30 @@ function loadJson() {
                         <h6 class="card-title">${dados[i].titulo}</h6>
                     </div>
                 </a>
-                <p class="tags">${dados[i].tags}</p>
+                <p class="tags">Tags: ${dados[i].tags}</p>
             </div>`
+            }
         })
 
 
     })
 }
+
+
+
+var iconBusca = document.querySelector(".iconBusca")
+var campo = document.querySelector(".campo")
+
+iconBusca.onclick = function(e) {
+    e.preventDefault()
+    campo.style.display == "block" ? campo.style.display = "none" : campo.style.display = "block"
+}
+
+
+/* 
+document.addEventListener('mouseup', function(e) {
+    e.preventDefault()
+    if (!campo.contains(e.target)) {
+        campo.style.display = "none"
+    }
+}) */
