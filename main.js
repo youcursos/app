a733445fd49913c9e0ed206de782fecf9f45f24f
youcursos <84543261+youcursos@users.noreply.github.com>
2021-06-12 13:48:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,28 +7,30 @@ function loadJson() {
         resposta.json().then(function(data) {
             let dados = data.contente
             var campoBusca = document.querySelector("#campo").value
-            recebe.innerHTML = ""
-            for (let i = 0; i < dados.length; i++)
-            /* Busca */
-                if (dados[i].tags.includes(campoBusca)) {
 
-                recebe.innerHTML += `<div class="cardCursos pb-1">
-                <a href="${dados[i].link}">
-                    <img src="${dados[i].linkImg}" class="card-img-top " alt="... ">
-                    <div class="card-body text-white d-flex justify-content-center align-itens-center ">
-                        <h6 class="card-title">${dados[i].titulo}</h6>
-                    </div>
-                </a>
-                <p class="tags">Tags: ${dados[i].tags}</p>
-            </div>`
+            campoBusca
+            recebe.innerHTML = ""
+            for (let i = 0; i < dados.length; i++) {
+                /* Busca */
+                if (dados[i].tags.includes(campoBusca.trim()) || dados[i].titulo.includes(campoBusca.trim())) {
+
+                    recebe.innerHTML += `<div class="cardCursos pb-1">
+                    <a href="${dados[i].link}">
+                        <img src="${dados[i].linkImg}" class="card-img-top " alt="...">
+                        <div class="card-body text-white d-flex justify-content-center align-itens-center ">
+                            <h6 class="card-title">${dados[i].titulo}</h6>
+                        </div>
+                    </a>
+                    <p class="tags">Tags: ${dados[i].tags}</p>
+                    </div>`
+
+                }
             }
         })
 
 
     })
 }
-
-
 
 var iconBusca = document.querySelector(".iconBusca")
 var campo = document.querySelector(".campo")
