ef0609173db1bab5c3132ac3c1d858c2f6b5e57e
youcursos <84543261+youcursos@users.noreply.github.com>
2021-06-16 17:27:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,20 +8,19 @@ function loadJson() {
             let dados = data.contente
             var campoBusca = document.querySelector("#campo").value
 
-            campoBusca
             recebe.innerHTML = ""
             for (let i = 0; i < dados.length; i++) {
                 /* Busca */
-                if (dados[i].tags.includes(campoBusca.trim()) || dados[i].titulo.includes(campoBusca.trim())) {
+                if (dados[i].tags.includes(campoBusca.trim().toLowerCase())) {
 
                     recebe.innerHTML += `<div class="cardCursos pb-1">
                     <a href="${dados[i].link}">
-                        <img src="${dados[i].linkImg}" class="card-img-top " alt="...">
+                        <img src="${dados[i].linkImg}" class="card-img-top" alt="${dados[i].titulo}">
                         <div class="card-body text-white d-flex justify-content-center align-itens-center ">
                             <h6 class="card-title">${dados[i].titulo}</h6>
                         </div>
                     </a>
-                    <p class="tags">Tags: ${dados[i].tags}</p>
+                    <p class="tags">Tags: ${dados[i].tags.slice(0, 25)}...</p>
                     </div>`
 
                 }
